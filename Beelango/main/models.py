89b28dc8444686894ef88db02e0c4e129e936d38
Komilast <Komilast400@gmail.com>
2023-05-27 20:08:28 +0300

    Написана модель слова

--- Beelango/main/models.py
+++ Beelango/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Word(models.Model):
+    english = models.CharField(max_length=100, verbose_name='английское слово')
+    russian = models.TextField(verbose_name='русские слова')
+    transcription = models.CharField(max_length=50, verbose_name='транскрипция')
+    synonyms = models.ManyToManyField('self', blank=True, verbose_name='синонимы')
+
+    def __str__(self):
+        return self.english
